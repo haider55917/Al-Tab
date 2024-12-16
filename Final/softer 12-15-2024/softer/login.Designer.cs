47434a669fe6031f02d9c4c7a9869abe979463d4
unknown <haider55917@gmail.com>
2024-12-16 10:10:54 +0300

    النسخة النهاية للبرنامج شاملة تغيرات التصميم بدون برمجة

---
+++ Final/softer 12-15-2024/softer/login.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace softer
+{
+    partial class login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(login));
+            this.panel_pic = new System.Windows.Forms.Panel();
+            this.Exit = new Bunifu.Framework.UI.BunifuTileButton();
+            this.Enter = new Guna.UI2.WinForms.Guna2Button();
+            this.pass = new System.Windows.Forms.TextBox();
+            this.username = new System.Windows.Forms.TextBox();
+            this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.bunifuElipse2 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.bunifuElipse3 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.panel_pic.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel_pic
+            // 
+            this.panel_pic.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel_pic.BackgroundImage")));
+            this.panel_pic.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel_pic.Controls.Add(this.Exit);
+            this.panel_pic.Controls.Add(this.Enter);
+            this.panel_pic.Controls.Add(this.pass);
+            this.panel_pic.Controls.Add(this.username);
+            this.panel_pic.Location = new System.Drawing.Point(-2, 0);
+            this.panel_pic.Name = "panel_pic";
+            this.panel_pic.Size = new System.Drawing.Size(555, 545);
+            this.panel_pic.TabIndex = 0;
+            // 
+            // Exit
+            // 
+            this.Exit.BackColor = System.Drawing.Color.Transparent;
+            this.Exit.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Exit.BackgroundImage")));
+            this.Exit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Exit.color = System.Drawing.Color.Transparent;
+            this.Exit.colorActive = System.Drawing.Color.Transparent;
+            this.Exit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Exit.Font = new System.Drawing.Font("Century Gothic", 15.75F);
+            this.Exit.ForeColor = System.Drawing.Color.White;
+            this.Exit.Image = null;
+            this.Exit.ImagePosition = 20;
+            this.Exit.ImageZoom = 50;
+            this.Exit.LabelPosition = 41;
+            this.Exit.LabelText = "";
+            this.Exit.Location = new System.Drawing.Point(6, 6);
+            this.Exit.Margin = new System.Windows.Forms.Padding(6);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(45, 43);
+            this.Exit.TabIndex = 12;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // Enter
+            // 
+            this.Enter.AccessibleRole = System.Windows.Forms.AccessibleRole.Window;
+            this.Enter.Animated = true;
+            this.Enter.AutoRoundedCorners = true;
+            this.Enter.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(35)))), ((int)(((byte)(30)))));
+            this.Enter.BorderRadius = 16;
+            this.Enter.CheckedState.Parent = this.Enter;
+            this.Enter.CustomImages.Parent = this.Enter;
+            this.Enter.FillColor = System.Drawing.Color.Teal;
+            this.Enter.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Enter.ForeColor = System.Drawing.Color.White;
+            this.Enter.HoverState.Parent = this.Enter;
+            this.Enter.Location = new System.Drawing.Point(217, 427);
+            this.Enter.Name = "Enter";
+            this.Enter.ShadowDecoration.Parent = this.Enter;
+            this.Enter.Size = new System.Drawing.Size(116, 34);
+            this.Enter.TabIndex = 11;
+            this.Enter.Text = "تسجيل الدخول";
+            this.Enter.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+            this.Enter.Click += new System.EventHandler(this.Enter_Click);
+            // 
+            // pass
+            // 
+            this.pass.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pass.Location = new System.Drawing.Point(205, 390);
+            this.pass.Multiline = true;
+            this.pass.Name = "pass";
+            this.pass.Size = new System.Drawing.Size(142, 28);
+            this.pass.TabIndex = 1;
+            // 
+            // username
+            // 
+            this.username.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.username.Location = new System.Drawing.Point(205, 358);
+            this.username.Multiline = true;
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(142, 28);
+            this.username.TabIndex = 0;
+            // 
+            // bunifuElipse1
+            // 
+            this.bunifuElipse1.ElipseRadius = 25;
+            this.bunifuElipse1.TargetControl = this.username;
+            // 
+            // bunifuElipse2
+            // 
+            this.bunifuElipse2.ElipseRadius = 25;
+            this.bunifuElipse2.TargetControl = this.pass;
+            // 
+            // bunifuElipse3
+            // 
+            this.bunifuElipse3.ElipseRadius = 25;
+            this.bunifuElipse3.TargetControl = this;
+            // 
+            // login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(65)))), ((int)(((byte)(51)))));
+            this.ClientSize = new System.Drawing.Size(553, 546);
+            this.Controls.Add(this.panel_pic);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel_pic.ResumeLayout(false);
+            this.panel_pic.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel_pic;
+        private System.Windows.Forms.TextBox pass;
+        private System.Windows.Forms.TextBox username;
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse2;
+        private Guna.UI2.WinForms.Guna2Button Enter;
+        private Bunifu.Framework.UI.BunifuTileButton Exit;
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse3;
+    }
+}
---
+++ Final/softer 12-15-2024/softer/tableg.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace softer
+{
+    public partial class tableg : Form
+    {
+        public tableg()
+        {
+            InitializeComponent();
+        }
+
+        private void btnadd_Click(object sender, EventArgs e)
+        {
+            if (!tabControl1.TabPages.Contains(Form_astqtab1))
+            {
+                tabControl1.Visible = true;
+                tabControl1.Dock = DockStyle.Fill;
+                tabControl1.TabPages.Add(Form_astqtab1);
+
+                home home = new home();
+                var x = Application.OpenForms["home"] as home;
+                x.btn_slide_main.Enabled = false;
+            }
+            tabControl1.SelectedTab = Form_astqtab1;
+            //MessageBox.Show("hhhh");
+          
+            
+        }
+
+        private void tableg_Load(object sender, EventArgs e)
+        {
+            tabControl1.Visible = false;
+            tabControl1.TabPages.Clear();
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnnext2_Click(object sender, EventArgs e)
+        {
+            if (!tabControl1.TabPages.Contains(Form_astqtab3))
+            {
+                tabControl1.TabPages.Add(Form_astqtab3);
+                tabControl1.SelectedTab = Form_astqtab3;
+            }
+            tabControl1.SelectedTab = Form_astqtab3;
+        }
+
+        private void transfer_Click(object sender, EventArgs e)
+        {
+            if (!tabControl1.TabPages.Contains(Form_astqtab4))
+            {
+                tabControl1.TabPages.Add(Form_astqtab4);
+                tabControl1.SelectedTab = Form_astqtab4;
+            }
+            tabControl1.SelectedTab = Form_astqtab4;
+        }
+
+        private void transfer_out_Click(object sender, EventArgs e)
+        {
+            if (!tabControl1.TabPages.Contains(Form_astqtab6))
+            {
+                tabControl1.TabPages.Add(Form_astqtab6);
+                tabControl1.SelectedTab = Form_astqtab6;
+            }
+            tabControl1.SelectedTab = Form_astqtab6;
+        }
+
+        private void transfer_first_Click(object sender, EventArgs e)
+        {
+            if (!tabControl1.TabPages.Contains(Form_astqtab5))
+            {
+                tabControl1.TabPages.Add(Form_astqtab5);
+                tabControl1.SelectedTab = Form_astqtab5;
+            }
+            tabControl1.SelectedTab = Form_astqtab5;
+        }
+
+        private void transfer_thired_Click(object sender, EventArgs e)
+        {
+            if (!tabControl1.TabPages.Contains(Form_astqtab7))
+            {
+                tabControl1.TabPages.Add(Form_astqtab7);
+                tabControl1.SelectedTab = Form_astqtab7;
+            }
+            tabControl1.SelectedTab = Form_astqtab7;
+        }
+
+        private void btnsavelast_Click(object sender, EventArgs e)
+        {
+            home hom = (home)Application.OpenForms["home"];
+            if (hom != null)
+            {
+                hom.form_panel.Controls.Clear();
+
+                tableg tab = new tableg();
+                tab.TopLevel = false;
+                tab.FormBorderStyle = FormBorderStyle.None;
+                tab.Dock = DockStyle.Fill;
+                hom.form_panel.Controls.Add(tab);
+                tab.Show();
+            }
+        }
+
+        private void btnblas_Click(object sender, EventArgs e)
+        {
+            List<Panel> panelsToMove = new List<Panel> { panelwtha, panelcart, paneldwra, panelcarttd };
+            PanelExpander expander = new PanelExpander(panelaml, panelsToMove, btnblas, 150);
+            expander.ExpandPanel();
+        }
+
+        private void btnblas2_Click(object sender, EventArgs e)
+        {
+            List<Panel> panelsToMove = new List<Panel> { panelcart, paneldwra, panelcarttd };
+            PanelExpander expander = new PanelExpander(panelwtha, panelsToMove, btnblas2, 150);
+            expander.ExpandPanel();
+        }
+
+        private void btnblas3_Click(object sender, EventArgs e)
+        {
+            List<Panel> panelsToMove = new List<Panel> { paneldwra, panelcarttd };
+            PanelExpander expander = new PanelExpander(panelcart, panelsToMove, btnblas3, 150);
+            expander.ExpandPanel();
+        }
+
+        private void btnblas4_Click(object sender, EventArgs e)
+        {
+            List<Panel> panelsToMove = new List<Panel> { panelcarttd };
+            PanelExpander expander = new PanelExpander(paneldwra, panelsToMove, btnblas4, 150);
+            expander.ExpandPanel();
+        }
+
+        private void btnblas5_Click(object sender, EventArgs e)
+        {
+            // هنا تمرير Panel3 كـ scrollablePanel للتحكم في التمرير
+            PanelExpander expander = new PanelExpander(panelcarttd, new List<Panel>(), btnblas5, 150, paneltableg3);
+            expander.ExpandPanel();
+        }
+
+        private void btnblas6_Click(object sender, EventArgs e)
+        {
+            List<Panel> panelsToMove = new List<Panel> { };
+            PanelExpander expander = new PanelExpander(panalmqdmat, panelsToMove, btnblas6, 150);
+            expander.ExpandPanel();
+        }
+
+        private void btnblas7_Click(object sender, EventArgs e)
+        {
+            List<Panel> panelsToMove = new List<Panel> { };
+            PanelExpander expander = new PanelExpander(panelstoh1, panelsToMove, btnblas7, 150);
+            expander.ExpandPanel();
+        }
+
+        private void btnblas8_Click(object sender, EventArgs e)
+        {
+            List<Panel> panelsToMove = new List<Panel> { };
+            PanelExpander expander = new PanelExpander(panelstoh2, panelsToMove, btnblas8, 150);
+            expander.ExpandPanel();
+        }
+
+        private void btnblas9_Click(object sender, EventArgs e)
+        {
+            List<Panel> panelsToMove = new List<Panel> { };
+            PanelExpander expander = new PanelExpander(panelout, panelsToMove, btnblas9, 150);
+            expander.ExpandPanel();
+        }
+
+        private void btnblas10_Click(object sender, EventArgs e)
+        {
+            List<Panel> panelsToMove = new List<Panel> { };
+            PanelExpander expander = new PanelExpander(panelmoarfon, panelsToMove, btnblas10, 150);
+            expander.ExpandPanel();
+        }
+
+        private void scanpic_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files |*.jpg;*.jpeg;*.png;*.bmb";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                pic_scan_moa.Image = Image.FromFile(openFileDialog.FileName);
+                pic_scan_moa.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label57_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox47_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btn_pic_info_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files |*.jpg;*.jpeg;*.png;*.bmb";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                pictureinfo.Image = Image.FromFile(openFileDialog.FileName);
+                pictureinfo.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+        }
+
+        private void btnnext_Click(object sender, EventArgs e)
+        {
+            if (!tabControl1.TabPages.Contains(Form_astqtab2))
+            {
+                tabControl1.TabPages.Add(Form_astqtab2);
+                tabControl1.SelectedTab = Form_astqtab2;
+            }
+            tabControl1.SelectedTab = Form_astqtab2;
+        }
+
+        private void btntadqeq_Click(object sender, EventArgs e)
+        {
+            if (!tabControl1.TabPages.Contains(Form_astqtab8))
+            {
+                tabControl1.TabPages.Add(Form_astqtab8);
+                tabControl1.SelectedTab = Form_astqtab8;
+                tabControl1.Visible = true;
+                tabControl1.Dock = DockStyle.Fill;
+                home home = new home();
+                var x = Application.OpenForms["home"] as home;
+                x.btn_slide_main.Enabled = false;
+            }
+            tabControl1.SelectedTab = Form_astqtab8;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("ستتوفر قريباً");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            MessageBox.Show("ستتوفر قريباً");
+        }
+
+        private void btntkarer_Click(object sender, EventArgs e)
+        {
+
+            MessageBox.Show("ستتوفر قريباً");
+        }
+
+        private void button2_Click_2(object sender, EventArgs e)
+        {
+
+            home home = new home();
+            home.Show();
+            this.Close();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+            if (MessageBox.Show("هل انت متأكد من الرجوع", "تحذير", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+            {
+                home hom = (home)Application.OpenForms["home"];
+                if (hom != null)
+                {
+                    hom.form_panel.Controls.Clear();
+
+                    tableg tab = new tableg();
+                    tab.TopLevel = false;
+                    tab.FormBorderStyle = FormBorderStyle.None;
+                    tab.Dock = DockStyle.Fill;
+                    hom.form_panel.Controls.Add(tab);
+                    tab.Show();
+                }
+            }
+    }
+
+        private void btn_back_Click(object sender, EventArgs e)
+        {
+            home hom = (home)Application.OpenForms["home"];
+            if (hom != null)
+            {
+                hom.form_panel.Controls.Clear();
+
+                tableg tab = new tableg();
+                tab.TopLevel = false;
+                tab.FormBorderStyle = FormBorderStyle.None;
+                tab.Dock = DockStyle.Fill;
+                hom.form_panel.Controls.Add(tab);
+                tab.Show();
+            }
+        }
+    }
+}
